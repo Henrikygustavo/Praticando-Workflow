c8a00d883178ba42feb98bdba21b46156969583d
Gustavo Henriky Ferreira Bernardes <140988394+Henrikygustavo@users.noreply.github.com>
2025-02-23 23:00:22 -0300

    Update Task.php

--- src/Task.php
+++ src/Task.php
@@ -4,12 +4,31 @@ class Task
 {
     private string $title;
     private string $description;
+    private bool $completed = false;
 
     public function __construct(string $title, string $description)
     {
         $this->title = $title;
         $this->description = $description;
     }
-}
 
-?>
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function markAsCompleted(): void
+    {
+        $this->completed = true;
+    }
+
+    public function isCompleted(): bool
+    {
+        return $this->completed;
+    }
+}
