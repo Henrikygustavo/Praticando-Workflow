3fd4b5f9beedd69aacadf9c40974b20d45704f19
Gustavo Henriky Ferreira Bernardes <140988394+Henrikygustavo@users.noreply.github.com>
2025-02-23 22:48:54 -0300

    Update TaskManagerTest.php

--- tests/TaskManagerTest.php
+++ tests/TaskManagerTest.php
@@ -1,34 +1,14 @@
 <?php
-
 use PHPUnit\Framework\TestCase;
-
 require_once "src/TaskManager.php";
 
-class TaskManagerTest extends TestCase {
-    private $taskManager;
-
-    protected function setUp(): void {
-        $this->taskManager = new TaskManager();
-    }
-
-    public function testAddTask() {
-        $taskId = $this->taskManager->addTask("Nova Tarefa");
-        $this->assertIsNumeric($taskId);
-    }
-
-    public function testGetTasks() {
-        $tasks = $this->taskManager->getTasks();
-        $this->assertIsArray($tasks);
-    }
-
-    public function testMarkAsCompleted() {
-        $taskId = $this->taskManager->addTask("Tarefa Teste");
-        $this->assertTrue($this->taskManager->markAsCompleted($taskId));
-    }
-
-    public function testDeleteTask() {
-        $taskId = $this->taskManager->addTask("Tarefa para Deletar");
-        $this->assertTrue($this->taskManager->deleteTask($taskId));
+class TaskManagerTest extends TestCase
+{
+    public function testAdicionarTarefa()
+    {
+        $taskManager = new TaskManager();
+        $taskManager->addTask("Estudar PHP");
+        
+        $this->assertCount(1, $taskManager->getTasks());
     }
 }
-?>
